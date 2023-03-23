8ce07e17a9db56bf374182bfc0175f8946e0ec57
Alan Sanchez <alaansannchezz@gmail.com>
2023-03-23 17:37:57 -0300

    feat: Se integro el sistema de Autenticacion

---
+++ app/Notifications/AccountConfirmation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AccountConfirmation extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public User $user)
+    {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = url('/api/confirm/' . $this->user->confirmation_token);
+
+        return (new MailMessage)
+            ->line('Hola ' . $this->user->name . ' ' . $this->user->lastname . ',')
+            ->line('Gracias por registrarte en nuestra aplicación. Por favor confirma tu cuenta haciendo clic en el siguiente enlace:')
+            ->action('Confirmar cuenta', $url)
+            ->line('Gracias por usar nuestra aplicación!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('/register', 'register')->name('register');
+    Route::get('/confirm/{token}', 'confirm');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
